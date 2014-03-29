d8766963d258cddd9257d315ec382ec436c2e0cc
Biont <arbelzapf@gmail.com>
2014-03-29 16:04:34 +0100

    Initial commit

---
+++ admin/views/admin.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ */
+?>
+
+<div class="wrap">
+
+    <h2><?php echo esc_html(get_admin_page_title()); ?></h2>
+    <form method="post">
+        <?php settings_fields('playful-wp-settings'); ?>
+        <?php do_settings_sections('playful-wp-settings'); ?>
+        <table class="form-table">
+            <tr valign="top">
+                <th scope="row">New Option Name</th>
+                <td><input type="text" name="new_option_name" value="<?php echo get_option('new_option_name'); ?>" /></td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">Some Other Option</th>
+                <td><input type="text" name="some_other_option" value="<?php echo get_option('some_other_option'); ?>" /></td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">Options, Etc.</th>
+                <td><input type="text" name="option_etc" value="<?php echo get_option('option_etc'); ?>" /></td>
+            </tr>
+        </table>
+
+        <?php submit_button(); ?>
+
+    </form>
+    <!-- @TODO: Provide markup for your options page here. -->
+
+</div>
---
+++ playful-wp.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The WordPress Plugin Boilerplate.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @package   Plugin_Name
+ * @author    Your Name <email@example.com>
+ * @license   GPL-2.0+
+ * @link      http://example.com
+ * @copyright 2014 Your Name or Company Name
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Playful-WP
+ * Plugin URI:        @TODO
+ * Description:       A plugin that extends WordPress with several gamification features like character-creation, achievements, quests and inventories.
+ * Version:           1.0.0
+ * Author:            Moritz Meißelbach
+ * Author URI:        http://www.github.com/Biont
+ * Text Domain:       plugin-name-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/<owner>/<repo>
+ * WordPress-Plugin-Boilerplate: v2.6.1
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace `class-plugin-name.php` with the name of the plugin's class file
+ *
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'public/PlayfulWP.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ */
+register_activation_hook( __FILE__, array( 'PlayfulWP', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'PlayfulWP', 'deactivate' ) );
+
+/*
+ * @TODO:
+ *
+ * - replace Plugin_Name with the name of the class defined in
+ *   `class-plugin-name.php`
+ */
+add_action( 'plugins_loaded', array( 'PlayfulWP', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ *
+ * If you want to include Ajax within the dashboard, change the following
+ * conditional to:
+ *
+ * if ( is_admin() ) {
+ *   ...
+ * }
+ *
+ * The code below is intended to to give the lightest footprint possible.
+ */
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/PlayfulWPAdmin.php' );
+	add_action( 'plugins_loaded', array( 'PlayfulWPAdmin', 'get_instance' ) );
+
+}
